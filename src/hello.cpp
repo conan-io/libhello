3cafb6d68f0b9e596468c2831b31e05ac41dc0d3
czoido <mrgalleta@gmail.com>
2022-06-13 17:23:31 +0200

    wip

--- src/hello.cpp
+++ src/hello.cpp
@@ -28,61 +28,61 @@ void hello(){
 
     // Libstdc++
     #if defined _GLIBCXX_USE_CXX11_ABI
-    fmt::print(fg(fmt::color::crimson), "  hello/1.0: _GLIBCXX_USE_CXX11_ABI "<< _GLIBCXX_USE_CXX11_ABI << "\n");
+    fmt::print(fg(fmt::color::crimson), "  hello/1.0: _GLIBCXX_USE_CXX11_ABI {}\n", _GLIBCXX_USE_CXX11_ABI);
     #endif
 
     // COMPILER VERSIONS
     #if _MSC_VER
-    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: _MSC_VER" << _MSC_VER<< "\n");
+    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: _MSC_VER {}\n", _MSC_VER);
     #endif
 
     #if _MSVC_LANG
-    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: _MSVC_LANG" << _MSVC_LANG<< "\n");
+    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: _MSVC_LANG {}\n", _MSVC_LANG);
     #endif
 
     #if __cplusplus
-    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __cplusplus" << __cplusplus<< "\n");
+    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __cplusplus {}\n", __cplusplus);
     #endif
 
     #if __INTEL_COMPILER
-    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __INTEL_COMPILER" << __INTEL_COMPILER<< "\n");
+    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __INTEL_COMPILER {}\n", __INTEL_COMPILER);
     #endif
 
     #if __GNUC__
-    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __GNUC__" << __GNUC__<< "\n");
+    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __GNUC__ {}\n", __GNUC__);
     #endif
 
     #if __GNUC_MINOR__
-    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __GNUC_MINOR__" << __GNUC_MINOR__<< "\n");
+    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __GNUC_MINOR__ {}\n", __GNUC_MINOR__);
     #endif
 
     #if __clang_major__
-    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __clang_major__" << __clang_major__<< "\n");
+    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __clang_major__ {}\n", __clang_major__);
     #endif
 
     #if __clang_minor__
-    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __clang_minor__" << __clang_minor__<< "\n");
+    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __clang_minor__ {}\n", __clang_minor__);
     #endif
 
     #if __apple_build_version__
-    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __apple_build_version__" << __apple_build_version__<< "\n");
+    fmt::print(fg(fmt::color::papaya_whip), "  hello/1.0: __apple_build_version__ {}\n", __apple_build_version__);
     #endif
 
     // SUBSYSTEMS
 
     #if __MSYS__
-    fmt::print(fg(fmt::color::spring_green), "  hello/1.0: __MSYS__" << __MSYS__<< "\n");
+    fmt::print(fg(fmt::color::spring_green), "  hello/1.0: __MSYS__ {}\n", __MSYS__);
     #endif
 
     #if __MINGW32__
-    fmt::print(fg(fmt::color::spring_green), "  hello/1.0: __MINGW32__" << __MINGW32__<< "\n");
+    fmt::print(fg(fmt::color::spring_green), "  hello/1.0: __MINGW32__ {}\n", __MINGW32__);
     #endif
 
     #if __MINGW64__
-    fmt::print(fg(fmt::color::spring_green), "  hello/1.0: __MINGW64__" << __MINGW64__<< "\n");
+    fmt::print(fg(fmt::color::spring_green), "  hello/1.0: __MINGW64__ {}\n", __MINGW64__);
     #endif
 
     #if __CYGWIN__
-    fmt::print(fg(fmt::color::spring_green), "  hello/1.0: __CYGWIN__" << __CYGWIN__<< "\n");
+    fmt::print(fg(fmt::color::spring_green), "  hello/1.0: __CYGWIN__ {}\n", __CYGWIN__);
     #endif
 }
