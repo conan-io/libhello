1a6e13c65af70f67eba711c29a69a04170255266
czoido <mrgalleta@gmail.com>
2022-08-01 12:29:33 +0200

    wip

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,9 +4,10 @@
 
 #include "gtest/gtest.h"
 namespace {
-
     TEST(HelloTest, ComposeMessages) {
       EXPECT_EQ(std::string("hello/1.0: Hello World Release! (with color!)\n"), compose_message("Release", "with color!"));
       EXPECT_EQ(std::string("hello/1.0: Hello World Debug! (with color!)\n"), compose_message("Debug", "with color!"));
+      EXPECT_EQ(std::string("hello/1.0: Hello World Release! (without color)\n"), compose_message("Release", "without color"));
+      EXPECT_EQ(std::string("hello/1.0: Hello World Debug! (without color)\n"), compose_message("Debug", "without color"));
     }
 }
