90415933adb55a93e6b07a8f61e68b04b20f7040
czoido <mrgalleta@gmail.com>
2022-08-01 12:30:26 +0200

    make fail

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,6 +8,6 @@ namespace {
       EXPECT_EQ(std::string("hello/1.0: Hello World Release! (with color!)\n"), compose_message("Release", "with color!"));
       EXPECT_EQ(std::string("hello/1.0: Hello World Debug! (with color!)\n"), compose_message("Debug", "with color!"));
       EXPECT_EQ(std::string("hello/1.0: Hello World Release! (without color)\n"), compose_message("Release", "without color"));
-      EXPECT_EQ(std::string("hello/1.0: Hello World Debug! (without color)\n"), compose_message("Debug", "without color"));
+      EXPECT_EQ(std::string("hello/1.0: Hellso World Debug! (without color)\n"), compose_message("Debug", "without color"));
     }
 }
