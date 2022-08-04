594942ea88a20876403a3497025ef4a3021b4e7a
czoido <mrgalleta@gmail.com>
2022-08-04 20:22:45 +0200

    minor changes

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,8 +1,7 @@
 #include "../include/hello.h"
 
-#include <limits.h>
-
 #include "gtest/gtest.h"
+
 namespace {
     TEST(HelloTest, ComposeMessages) {
       EXPECT_EQ(std::string("hello/1.0: Hello World Release! (with color!)\n"), compose_message("Release", "with color!"));
